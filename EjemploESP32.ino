402991523c186dbbce90b2da05d479a57d26f61a
Felipebjm <febejaranom@gmail.com>
2025-12-11 16:00:54 -0600

    Prints en la pantalla

--- EjemploESP32.ino
+++ EjemploESP32.ino
@@ -1,18 +1,27 @@
 // Requiere instalar las siguientes librerias:
 // - DHT Sensor Library 
 // - Adafruit Unified Sensor Lib
+// - TFT_eSPI
 
 #include "DHT.h"
+#include <TFT_eSPI.h>
 
-#define DHTPIN 15     // Pin conectado al sensor
+// --Pines--
+#define DHTPIN 15 
+#define LED_PIN 2
 
 #define DHTTYPE DHT11   
 
-#define LED_PIN 2
+//Por default se incializa el led encendido
+//False=encendido True=apagado
 bool estadoLED = false;
+
 // Inicializar el sensor DHT11
 // Se crea un objeto llamado dht de la clase DHT (clase incluida en la libreria)
 DHT dht(DHTPIN, DHTTYPE);
+
+//Inicializar la pantalla
+TFT_eSPI tft = TFT_eSPI(135, 240);
 
 void setup() {
   Serial.begin(9600); //La velocidad de comunicacion
@@ -21,22 +30,36 @@ void setup() {
   dht.begin();
 
   pinMode(LED_PIN, OUTPUT);
+  
+  // --Mensaje incial de la pantalla--
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextSize(2); 
+  tft.setCursor(0, 0);
+  tft.drawString("Hola mundo!", tft.width() / 2, tft.height() / 2 - 18);
+  tft.drawString("__________", tft.width() / 2, tft.height() / 2 );
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("Ejemplo DHT11", tft.width() / 2, tft.height() / 2 + 18);
+  tft.drawString(":D", tft.width() / 2, tft.height() / 2 + 50); 
 }
 
 void loop() {
    
-  // Timer de 2 segundo entre mediciones
-  delay(2000);
+  //Timer entre las mediciones y los prints
+  delay(500);
 
   estadoLED = !estadoLED;               // Cambia estado del led (encido o apagado)
   digitalWrite(LED_PIN, estadoLED);     // Lo aplica
 
-  //Lectura de datos del sensor
+  // --Lectura de datos del sensor--
   float humedad = dht.readHumidity();
   float temperaturaCel = dht.readTemperature();
   float temperaturaFah = dht.readTemperature(true);
 
-  // Verificacion y mensaje de error 
+  // --Verificacion y mensaje de error-- 
   if (isnan(humedad) || isnan(temperaturaCel) || isnan(temperaturaFah)) {
     Serial.println(F("Fallo al leer datos del sensor"));
     return;
@@ -46,7 +69,7 @@ void loop() {
   float indiceCel = dht.computeHeatIndex(temperaturaCel, humedad);
   float indiceFah = dht.computeHeatIndex(temperaturaFah, humedad, false);
 
-  // Prints
+  // --Prints de consola--
   Serial.print(F("Humedad: "));
   Serial.print(humedad);
   Serial.print(F("%  Temperatura: "));
@@ -58,9 +81,23 @@ void loop() {
   Serial.print(F("°C "));
   Serial.print(indiceFah);
   Serial.println(F("°F"));
+
   if(estadoLED == true){
     Serial.println("Led apagado");
   } else{
     Serial.println("led encencido");
   }
+
+  // --Prints de la pantalla--
+  tft.fillScreen(TFT_RED);
+  
+  tft.setTextColor(TFT_GREEN);
+  tft.drawString("Temperatura:", tft.width() / 2, 30);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString(String(temperaturaCel) + " C", tft.width() / 2, 50);
+
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("Humedad:", tft.width() / 2, 80);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString(String(humedad) + " %", tft.width() / 2, 100);
 }
